6a54ef95b1b63fe7b168e1b2b6eb4e0300111311
Greg Cerveny <greg.cerveny@gmail.com>
2014-01-06 14:13:16 -0600

    sending out logs

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var port = process.env.PORT || 3000;
 var io = require('socket.io').listen(+port);
 
 io.sockets.on('connection', function(socket){
-    io.sockets.emit('news', { hello: 'world'});
+    setInterval(function(){
+        var now = new Date();
+        var hex = (Math.random() + 1).toString(16).substring(5);
+        var entry = now.toLocaleTimeString() + ' ' + hex;
+        io.sockets.emit('log', entry);
+    }, 500);
     socket.on('my other event', function(data){
         console.log(data);
     })
