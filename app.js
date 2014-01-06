3ef6b9ca437b7c83fcb35588ac5ffada0786838c
Greg Cerveny <greg.cerveny@gmail.com>
2014-01-06 14:28:58 -0600

    adding reload

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var port = process.env.PORT || 3000;
 var io = require('socket.io').listen(+port);
 
+var generateHex = function() {
+    return (Math.random() + 1).toString(16).substring(5);
+}
+
+var generateLog = function() {
+    var now = new Date();
+    var entry = now.toLocaleTimeString() + ' ' + generateHex();
+    return entry;
+}
+
 io.sockets.on('connection', function(socket){
     setInterval(function(){
-        var now = new Date();
-        var hex = (Math.random() + 1).toString(16).substring(5);
-        var entry = now.toLocaleTimeString() + ' ' + hex;
-        io.sockets.emit('log', entry);
+        var entry = generateLog();
+        socket.emit('log', entry);
+
+        if (Math.floor(Math.random() * 20) == 0)
+            socket.emit('reload', {APP_ID:generateHex()});
     }, 500);
     socket.on('my other event', function(data){
         console.log(data);
